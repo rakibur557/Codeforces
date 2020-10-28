194aa962901cffed9b70a259c7c208a052c9d219
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2020-10-28 22:51:14 +0600

    Add files via upload

---
+++ Bit++.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,x=0;
+    cin>>n;
+    while(n--)
+    {
+        string s;
+        cin>>s;
+        if(s[1]=='+'){
+            ++x;
+        }
+        else
+            --x;
+    }
+    cout<<x<<endl;
+    return 0;
+}
