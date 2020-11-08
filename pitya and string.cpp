7f2f825e8850e5da48f88c8d88ced9a96d5eca9a
Rakibur Rahman <62113329+rakibur557@users.noreply.github.com>
2020-11-08 16:21:42 +0600

    Add files via upload

---
+++ pitya and string.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string.h>
+#include<algorithm>
+using namespace std;
+int main()
+{
+	string a, b;
+	cin >> a>>b;
+	for (int i = 0;i < a.length();i++)
+	{
+		if (a[i] >= 'A' && a[i] <= 'Z')
+			a[i] += 32;
+		if (b[i] >= 'A' && b[i] <= 'Z')
+			b[i] += 32;
+		if (a[i] > b[i]) { cout << "1"; return 0; }
+		if (a[i] < b[i]) { cout << "-1";return 0; }
+	}
+ 
+	cout << "0" << endl;
+	return 0;
+}
